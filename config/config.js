0919a4fa707313dd5cb5dbcf44e504beba1a82bb
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-03-07 11:20:45 -0700

    update DB Name

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: process.env.MYSQL_PASSWORD,
-    database: "passport_demo",
+    database: "delivery_db",
     host: "127.0.0.1",
     dialect: "mysql",
     operatorsAliases: false
@@ -10,7 +10,7 @@ module.exports = {
   test: {
     username: "root",
     password: process.env.MYSQL_PASSWORD,
-    database: "passport_demo",
+    database: "delivery_db",
     host: "127.0.0.1",
     dialect: "mysql",
     operatorsAliases: false
@@ -18,7 +18,7 @@ module.exports = {
   production: {
     username: "root",
     password: process.env.MYSQL_PASSWORD,
-    database: "passport_demo",
+    database: "delivery_db",
     host: "127.0.0.1",
     dialect: "mysql",
     operatorsAliases: false
